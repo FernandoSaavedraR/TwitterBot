68f4f06bd556f0788bfc49eea2463a31364bc7b2
Fernando Saavedra <50643518+FernandoSaavedraR@users.noreply.github.com>
2021-06-07 09:13:41 -0500

    config message

--- TwitterbotSRF/index.js
+++ TwitterbotSRF/index.js
@@ -1,6 +1,8 @@
+var today = new Date();
+var n = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()+' y son las '+today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 module.exports = async function (context, req) {
     context.log('Generating Nonsense...');
     context.res = {
-        body: "Bibble wibble wobble"
+        body: `Hola, hoy es ${n}`
     };
 };
